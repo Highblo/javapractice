ca442a148d9a3e992a479c7894222200f431cd7c
Highblo <nyaa.-525@outlook.jp>
2026-06-14 19:45:24 +0900

    変数とデータ型

--- src/First.java
+++ src/First.java
@@ -1,8 +1,24 @@
 public class First {
-    //Hello Worldの出力
     public static void main(String[] args) {
+        //Hello Worldの出力
         System.out.println("Hello World");
+
+        //変数
+        int month;
+        month = 6;
+        int day = 14;
+
+        System.out.println("今日は" + month + "月" + day + "日です");
+
+        //データ型
+        float weight = 70.3f;
+        double height = 175.5;
+        char bloodType = 'O';
+
+        String name = "○○";
+
+        System.out.println("私は" + name + "です");
+        System.out.println("身長は" + height + "cm、体重は" + weight + "kg");
+        System.out.println("血液型は" + bloodType + "型です");
     }
-
-
 }
