2b246fb0a45d727d771bbf0d4e6bc72cde422618
Highblo <nyaa.-525@outlook.jp>
2026-06-14 19:57:51 +0900

    配列

--- src/First.java
+++ src/First.java
@@ -10,7 +10,7 @@ public class First {
 
         System.out.println("今日は" + month + "月" + day + "日です");
 
-        //データ型
+        //基本データ型,参照型
         float weight = 70.3f;
         double height = 175.5;
         char bloodType = 'O';
@@ -20,5 +20,18 @@ public class First {
         System.out.println("私は" + name + "です");
         System.out.println("身長は" + height + "cm、体重は" + weight + "kg");
         System.out.println("血液型は" + bloodType + "型です");
+
+        //配列
+        int[] score = new int[3];
+        score[0] = 80;
+        score[1] = 100;
+        score[2] = 70;
+
+        String[] arrayName = {"田中", "佐藤" ,"山田"};
+
+        System.out.println(arrayName[0] + "さん：" + score[0] + "点");
+        System.out.println(arrayName[1] + "さん：" + score[1] + "点");
+        System.out.println(arrayName[2] + "さん：" + score[2] + "点");
+        System.out.println("受験者数：" + score.length + "名");
     }
 }
