c28c04d12c9ae87681ab7801e1e9d3b642c28d2f
Highblo <nyaa.-525@outlook.jp>
2026-06-22 19:41:54 +0900

    enzansi

--- src/First.java
+++ src/First.java
@@ -48,9 +48,29 @@ public class First {
         //javaコマンド実行時にargsにデータを渡せる
         System.out.println(args[0]);
         System.out.println(args[1]);
-        System.out.println(args[2]);
-        System.out.println(args[3]);
-        System.out.println(args[4]);
-        System.out.println(args.length);
+        // System.out.println(args[2]);
+        // System.out.println(args[3]);
+        // System.out.println(args[4]);
+        // System.out.println(args.length);
+
+        //演算子
+        //文字列を数値に変換
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        //算術演算子
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b)); // 割り切れない場合は切り捨て
+        System.out.println("a % b = " + (a % b));
+
+        //関係演算子
+        System.out.println("a >= 0 : " + (a >= 0));
+
+        //論理演算子
+        System.out.println("(a >= 0) && (b >= 0) = " + ((a >= 0) && (b >= 0)));
     }
+
 }
